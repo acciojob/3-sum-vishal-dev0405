31efc22eb1b4571668b523b5f8cc299768381ce2
Vishal Verma <100039120+vishal-dev0405@users.noreply.github.com>
2023-01-23 16:52:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 function threeSum(arr, target) {
-  //your code here
+  arr.sort((a, b) => a - b);
+    let closestSum = Infinity;
+    let closestNums;
+    for (let i = 0; i < arr.length - 2; i++) {
+        let j = i + 1;
+        let k = arr.length - 1;
+        while (j < k) {
+            let currentSum = arr[i] + arr[j] + arr[k];
+            if (currentSum === target) {
+                return currentSum;
+            } else if (Math.abs(currentSum - target) < Math.abs(closestSum - target)) {
+                closestSum = currentSum;
+                closestNums = [nums[i], nums[j], nums[k]];
+            }
+            if (currentSum > target) {
+                k--;
+            } else {
+                j++;
+            }
+        }
+    }
+    return closestSum;
 }
 
 module.exports = threeSum;
